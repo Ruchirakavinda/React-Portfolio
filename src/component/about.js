0fae158279e090444c355bc58ec8773fa763f75f
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-06-15 02:28:28 +0530

    about page modifiedt

--- src/component/about.js
+++ src/component/about.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import '../about.css';
+import myimg from '../img/myimg.jpg';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function About() {
     return(
         <div className='aboutmain'>
-            
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-4' style={{height:'200vh'}}>
+
+                    </div>
+                    <div className='col-4' style={{backgroundColor:'white', height:'200vh'}}>
+                       <img src={myimg} alt='My image'></img>
+                    </div>
+                    <div className='col-4' style={{backgroundColor:'blue', height:'200vh'}}>
+
+                    </div>
+
+                </div>
+
+            </div>
          </div>
     );
 }
