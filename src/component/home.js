8ab21596729d63ae0151ed147ce66f77618b21e9
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-09-29 00:31:11 +0530

    logo

--- src/component/home.js
+++ src/component/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/home.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MDBCol, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
+import logo from '../img/logo.png'
 
 function Home() {
     return(
@@ -11,7 +12,9 @@ function Home() {
             {/* <MDBCol lg="1" sm="0">
                <h1 className="bracket1">&#123;</h1> 
             </MDBCol> */}
-            <MDBCol  size="12" >
+            
+            <MDBCol  size="12" style={{textAlign:'center'}} >
+            <img src={logo} className="logoimg"/>
             <div className=" home">
                  <span className='iam'>Hi,  I'm </span>
                  <h1 className='name'>Ruchira <span className='lname'>Kavinda</span></h1><p className='skill'>Interested In Full Stack Developing,<br/> UI / UX & Graphic Designing</p>
