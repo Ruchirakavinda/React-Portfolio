93137c5c655b4eba8d1796b46cfc6fee0ccca6f6
Ruchirakavinda <ruchirakvnd@gmail.com>
2022-04-27 13:36:13 +0530

    about page update01

--- src/component/about.js
+++ src/component/about.js
@@ -28,30 +28,55 @@ function About() {
             </MDBContainer>
  
 
-            <MDBContainer fluid className='aboutrow2' >
+            <MDBContainer fluid className='aboutrow2'>
                <MDBContainer>
                     <MDBRow>
-                        <MDBCol lg='8' md='12' className='mx-auto'>
+                        <MDBCol lg='10' md='12' sm='12' className='mx-auto'>
                         <div className='crd1 mx-auto'>
-                            <h1 className='crdtopic'>Current</h1>
-                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula</p>
-                        </div>
-                        </MDBCol>
-                        <MDBCol lg='8'  md='12' className='mx-auto'>
-                        <div className='crd1 mx-auto'>
-                            <h1 className='crdtopic'>Current</h1>
-                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula</p>
-                        </div>
-                        </MDBCol>
-                        <MDBCol lg='8' md='12'  className='mx-auto'>
-                        <div className='crd1 mx-auto'>
-                            <h1 className='crdtopic'>Current</h1>
-                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula</p>
+                        <MDBContainer>
+                        <MDBRow>
+                            <MDBCol lg='6'>
+                            <h1 className='crdtopic' style={{color:"rgba(0, 153, 255, 0.767)"}}>I AM</h1>
+                            <p className='crdtopic'>PROFESSIONAL FULL-STACK DEVELOPER AND UI /UX AND GRAPHIC DESIGNER</p>
+                            </MDBCol>
+                            <MDBCol lg='6'>
+                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula</p>
+                            </MDBCol>
+                        </MDBRow>
+                        </MDBContainer>
                         </div>
                         </MDBCol>
                     </MDBRow>
 
                 </MDBContainer>
+
+                <h1 className='About_main_topic'> WHAT I <span style={{color:"rgba(0, 153, 255, 0.767)"}}>DO</span>...</h1>
+                
+                <MDBContainer>
+                    <MDBRow className='d-flex justify-content-center'>
+                        <MDBCol lg='4' md='6' sm='12' className='d-flex justify-content-center'>
+                            <div className='wid_crd1'>
+                            <h1 className='crdtopic'>Full-Stack Development</h1>
+                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula. Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </MDBCol>
+                        <MDBCol lg='4' md='6' sm='12' className='d-flex justify-content-center'>
+                            <div className='wid_crd2'>
+                            <h1 className='crdtopic'>UI / UX Design</h1>
+                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula. Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </MDBCol>
+                        <MDBCol lg='4' md='6' sm='12' className='d-flex justify-content-center'>
+                            <div className='wid_crd1'>
+                            <h1 className='crdtopic'>Graphic Design</h1>
+                            <p className='aboutcont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, ipsum condimentum ultricies vehicula. Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+
+                
+                <h1 className='About_main_topic'> WHAT I <span style={{color:"rgba(0, 153, 255, 0.767)"}}>DO</span>...</h1>
             </MDBContainer>
 
        </>
