b53b37a6b77252638e33a957237ddc88829620c5
Ruchirakavinda <ruchirakvnd@gmail.com>
2022-04-28 23:07:35 +0530

    about page update02

--- src/component/about.js
+++ src/component/about.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../css/about.css';
 import myimg from '../img/myimg4.png';
+import tech from '../img/tech.png';
 import { MDBContainer, MDBRow ,MDBCol } from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
 
 
 function About() {
@@ -76,7 +78,112 @@ function About() {
                 </MDBContainer>
 
                 
-                <h1 className='About_main_topic'> WHAT I <span style={{color:"rgba(0, 153, 255, 0.767)"}}>DO</span>...</h1>
+                <MDBContainer fluid style={{padding:'0%'}}>
+                    <MDBRow className='About_img_row' >
+                        <img src={tech} alt='used technolgies'></img>
+                    </MDBRow>
+                </MDBContainer>
+
+                <h1 className='About_main_topic'> OTHER SKILLS <span style={{color:"rgba(0, 153, 255, 0.767)"}}>AND</span> TALENTS</h1>
+
+                <MDBContainer className='main2_cont02'>
+
+                    <MDBRow>
+                    <MDBCol className='Home_service_col d-flex justify-content-center' lg='6' md='12' sm='12' >
+                        <div className='shaped_div_fs'></div>
+                        <div className='shaped_div_two'></div>
+                    </MDBCol>
+                    <MDBCol className='Home_service_col' lg='6' md='0' sm='0' >
+                        <h2 className='sub_topic'>Photography and Retouching</h2>
+                        <p className='cont'>Nulla occaecat in voluptate excepteur cupidatat incididunt ut anim dolor. Ex reprehenderit cillum id exercitation eu fugiat laborum adipisicing excepteur duis. Et nisi non proident cillum anim labore. Aliquip non anim ad quis incididunt cupidatat ut est reprehenderit in nisi. 
+                        </p>
+
+                        <Link to="/project"> <div className='service_btn'> More </div></Link>
+                    </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow>
+                    <MDBCol className='Home_service_col ' lg='6' md='12' sm='12' >
+                    <h2 className='sub_topic'>Drawing and Painting</h2>
+                        <p className='cont'>Nulla occaecat in voluptate excepteur cupidatat incididunt ut anim dolor. Ex reprehenderit cillum id exercitation eu fugiat laborum adipisicing excepteur duis. Et nisi non proident cillum anim labore. Aliquip non anim ad quis incididunt cupidatat ut est reprehenderit in nisi.
+                        </p>
+                        <Link to="/project"> <div className='service_btn'> More </div></Link>
+                        
+                    </MDBCol>
+                    <MDBCol className='Home_service_col d-flex justify-content-center' lg='6' md='12' sm='12' >
+                    <div className='shaped_div_two' style={{marginRight:"30px"}}></div>
+                    <div className='shaped_div_uiux'></div>
+                    </MDBCol>
+                    </MDBRow>
+
+
+                 </MDBContainer>
+
+     {/*//////////////mobile view/////////////*/}
+
+                 <MDBContainer className='main2_cont01_mobile'>
+
+                    <MDBRow>
+                    <MDBCol className='Home_service_col' lg='6' md='12' sm='12' >
+                    <h2 className='sub_topic_mob'>Photography and Photo Retouching</h2>
+                        <p className='cont_mob'>Nulla occaecat in voluptate excepteur cupidatat incididunt ut anim dolor. Ex reprehenderit cillum id exercitation eu fugiat laborum adipisicing excepteur duis. Et nisi non proident cillum anim labore. Aliquip non anim ad quis incididunt cupidatat ut est reprehenderit in nisi. 
+                        </p>
+                        <Link to="/project" className='d-flex justify-content-center'> <div className='service_btn'> More </div></Link>
+                        
+                    </MDBCol>
+                    <MDBCol className='Home_service_col d-flex justify-content-center' lg='6' md='12' sm='12' >
+                    <div className='shaped_div_two' style={{marginRight:"30px",marginLeft:"-5px"}}></div>
+                    <div className='shaped_div_fs'></div>
+                    </MDBCol>
+                    </MDBRow>
+
+
+                    <MDBRow>
+                    <MDBCol className='Home_service_col' lg='6' md='12' sm='12' >
+                    <h2 className='sub_topic_mob'>Drawing and Painting</h2>
+                        <p className='cont_mob'>Nulla occaecat in voluptate excepteur cupidatat incididunt ut anim dolor. Ex reprehenderit cillum id exercitation eu fugiat laborum adipisicing excepteur duis. Et nisi non proident cillum anim labore. Aliquip non anim ad quis incididunt cupidatat ut est reprehenderit in nisi. 
+                        </p>
+                        <Link to="/project" className='d-flex justify-content-center'> <div className='service_btn'> More </div></Link>
+                        
+                    </MDBCol>
+                    <MDBCol className='Home_service_col d-flex justify-content-center' lg='6' md='12' sm='12' >
+                    <div className='shaped_div_two' style={{marginRight:"30px",marginLeft:"-5px"}}></div>
+                    <div className='shaped_div_uiux'></div>
+                    </MDBCol>
+                    </MDBRow>
+
+
+                </MDBContainer>
+
+
+
+
+
+
+
+                {/* <MDBContainer className='About_otherskill_row' >
+                  
+                        <h1 className='crdtopic'>Graphic Design</h1>
+                        <p className='aboutcont'>Aliquip nulla cupidatat tempor dolore fugiat velit officia exercitation pariatur ea. Sint mollit cillum magna labore consequat quis minim labore enim. Occaecat ipsum consequat in occaecat officia eu laboris ex.Irure ullamco pariatur deserunt pariatur est excepteur. Dolore veniam magna ut esse enim deserunt consequat irure nulla. Duis aliqua dolore irure laborum elit nostrud nulla excepteur tempor fugiat occaecat velit est. </p>
+                   
+                        <Link to="/project"> <div className='service_btn'> More </div></Link>
+                </MDBContainer>     
+
+                <MDBContainer  className='About_otherskill_row'>
+                  
+                        <h1 className='crdtopic'>Graphic Design</h1>
+                        <p className='aboutcont'>Aliquip nulla cupidatat tempor dolore fugiat velit officia exercitation pariatur ea. Sint mollit cillum magna labore consequat quis minim labore enim. Occaecat ipsum consequat in occaecat officia eu laboris ex.Irure ullamco pariatur deserunt pariatur est excepteur. Dolore veniam magna ut esse enim deserunt consequat irure nulla. Duis aliqua dolore irure laborum elit nostrud nulla excepteur tempor fugiat occaecat velit est. </p>
+                   
+                </MDBContainer>            */}
+
+
+            <div className='Home_end_row'>
+               <h1 className='Home_end_slog'> <span style={{color:"rgba(0, 153, 255, 0.767)"}}>So</span>, Let's Get Started!</h1>
+               <div className='d-flex justify-content-center' >
+                  <Link to="/contact"><button className='contact_btn mx-auto'>Contact Me</button></Link>
+               </div>
+            </div>
+
             </MDBContainer>
 
        </>
